d5a7845a03adb6e966ee17c9fe553f4787a25e07
Giovanni <gio.a.98@gmail.com>
2021-12-13 11:57:15 +0100

    add base layout

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <header>
+      <div class="logo">
+        <img src="../assets/img/la-molisana-logo.png" alt="">
+      </div>
+      <nav>
+          <ul>
+              <li>
+                  <a href="#">Link</a>
+              </li>
+              <li>
+                  <a href="#">Link</a>
+              </li>
+              <li>
+                  <a href="#">Link</a>
+              </li>
+              <li>
+                  <a href="#">Link</a>
+              </li>
+          </ul>
+      </nav>
+  </header>
+</template>
+
+<script>
+export default {
+    name: "Header",
+}
+</script>
+
+<style>
+
+</style>
